8df535b567ce21536b1b8eec1685ebb8276fced0
Yudistira Maulana <yudistir4maulana@gmail.com>
2023-07-28 19:01:05 +0700

    fix hero img

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -82,7 +82,7 @@ const Header = ({ activeHeading }) => {
               className="absolute right-2 top-1.5 cursor-pointer"
             />
             {searchData && searchTerm && searchData.length !== 0 ? (
-              <div className="w-full absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4">
+              <div className="w-full absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-20 p-4">
                 {searchData &&
                   searchData.map((i, index) => {
                     return (
--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -1,34 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from "../../../styles/styles";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../../../styles/styles';
+import heroImg from '../../../Assests/image/hero.jpg';
 
 const Hero = () => {
   return (
     <div
-      className={`relative min-h-[50vh] min-h-[60vh] w-full bg-no-repeat ${styles.noramlFlex}`}
+      className={`relative min-h-[50vh]  w-full bg-no-repeat ${styles.noramlFlex}`}
       style={{
-        backgroundImage:
-          "url(https://mail3.pasarjaya.co.id/frontend/assets/img/banner/brt-1.jpg)",
+        backgroundImage: `url(${heroImg})`,
       }}
     >
       <div
         className="absolute inset-0 bg-black opacity-50"
         style={{ zIndex: 1 }}
       ></div>
-      <div className={`${styles.section} w-[90%] 800px:w-[60%]`} style={{ zIndex: 3 }}>
+      <div
+        className={`${styles.section} w-[90%] 800px:w-[60%]`}
+        style={{ zIndex: 3 }}
+      >
         <h1
           className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#eeeeee] font-[600] capitalize `}
         >
           Pasar Cidodol
         </h1>
         <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#dbdbdb]">
-          Tempat berbelanja bahan pangan terbaik <br /> Menyediakan mulai dari sayur segar, buah segar, protein, hingga bumbu dapur{" "}
-          <br /> 
+          Tempat berbelanja bahan pangan terbaik <br /> Menyediakan mulai dari
+          sayur segar, buah segar, protein, hingga bumbu dapur <br />
         </p>
         <Link to="/products" className="inline-block">
           <div className={`${styles.button} mt-5`}>
             <span className="text-[#ffffff] font-[Poppins] text-[18px]">
-              Belanja ! 
+              Belanja !
             </span>
           </div>
         </Link>
