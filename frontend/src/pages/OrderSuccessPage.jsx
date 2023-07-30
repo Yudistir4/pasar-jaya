b57f841027746bcf658e3781bef9bc88b4f1dcb7
Mochamad Haviz Tasmara <haviz@intern.hypefast.id>
2023-07-31 03:36:55 +0700

    update redirect and update order success

--- frontend/src/pages/OrderSuccessPage.jsx
+++ frontend/src/pages/OrderSuccessPage.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
 import Lottie from "react-lottie";
 import animationData from "../Assests/animations/107043-success.json";
+import { server } from "../server";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const OrderSuccessPage = () => {
   return (
@@ -15,6 +18,36 @@ const OrderSuccessPage = () => {
 };
 
 const Success = () => {
+  const { user } = useSelector((state) => state.user);
+
+  const orderData = JSON.parse(localStorage.getItem("latestOrder"));
+  const order = {
+    cart: orderData?.cart,
+    shippingAddress: orderData?.shippingAddress,
+    user: user && user,
+    totalPrice: orderData?.totalPrice,
+  }
+
+  const paymentHandler = async () => {
+    if (JSON.parse(localStorage.getItem('Pembayaran')).transaction_status === "settlement") {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      await axios
+        .post(`${server}/order/create-order`, order, config)
+        .then((res) => {
+          localStorage.setItem("cartItems", JSON.stringify([]));
+          localStorage.setItem("latestOrder", JSON.stringify([]));
+          localStorage.setItem("Pembayaran", JSON.stringify([]));
+          localStorage.setItem("tokenPayment", JSON.stringify([]));
+          window.location.reload();
+        });
+    }
+  }
+
   const defaultOptions = {
     loop: false,
     autoplay: true,
@@ -29,6 +62,7 @@ const Success = () => {
       <h5 className="text-center mb-14 text-[25px] text-[#000000a1]">
         Your order is successful 😍
       </h5>
+      <button onClick={paymentHandler} >Success</button>
       <br />
       <br />
     </div>
